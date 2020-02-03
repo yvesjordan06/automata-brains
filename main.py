a654fcb643d6e46b4b64b2f1d253bc8bbfab3f07
yvesjordan06 <yvesjordan06@gmail.com>
2020-02-03 08:31:11 +0100

    possibilite de demarrer a partir du terminal

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Models.Automate import Automate
 from Models.Transition import Transition
 from Models.Etat import Etat
 from Models.Alphabet import Alphabet
+from Views.main import run_app
 
 if __name__ == '__main__':
-    pass
+    run_app()
